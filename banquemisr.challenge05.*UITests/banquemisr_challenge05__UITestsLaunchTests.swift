6dfcf630304d0d244e878d93cf3e7599531d7a26
Marco Zakaria <marcozakaria@yahoo.com>
2024-09-26 23:41:37 +0300

    Initial Commit

---
+++ banquemisr.challenge05.*UITests/banquemisr_challenge05__UITestsLaunchTests.swift
@@ -0,0 +1,32 @@
+//
+//  banquemisr_challenge05__UITestsLaunchTests.swift
+//  banquemisr.challenge05.*UITests
+//
+//  Created by Marco on 2024-09-26.
+//
+
+import XCTest
+
+final class banquemisr_challenge05__UITestsLaunchTests: XCTestCase {
+
+    override class var runsForEachTargetApplicationUIConfiguration: Bool {
+        true
+    }
+
+    override func setUpWithError() throws {
+        continueAfterFailure = false
+    }
+
+    func testLaunch() throws {
+        let app = XCUIApplication()
+        app.launch()
+
+        // Insert steps here to perform after app launch but before taking a screenshot,
+        // such as logging into a test account or navigating somewhere in the app
+
+        let attachment = XCTAttachment(screenshot: app.screenshot())
+        attachment.name = "Launch Screen"
+        attachment.lifetime = .keepAlways
+        add(attachment)
+    }
+}
